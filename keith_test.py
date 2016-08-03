a000ed1a5805816fb4c3b6372b46d56b5c5d0732
mmcivor <mmcivor@belfastmet.ac.uk>
2016-08-03 16:23:03 +0100

    My Django Girls App, first commit

---
+++ keith_test.py
@@ -0,0 +1,7 @@
+def one():
+    print(1)
+def two():
+    print(2)
+if __name__ == "__main__":
+    one()
+    two()
---
+++ python_intro.py
@@ -0,0 +1,10 @@
+#def hi(name):
+#    print('Hello '+ name +'!')
+#    print('Hi ' + name +'!')
+#
+#girls = ['Ann', 'Joe', 'Linda', 'Marie']
+#for name in girls:
+#    hi(name)
+#    print('Next')
+for i in range(1,6):
+    print(i)
